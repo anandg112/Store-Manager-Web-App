cdab9cf31ddd7c328f7e219db0fa5e613803715c
anandg112 <anandg112@gmail.com>
2017-06-21 21:53:47 -0400

    implemented pagination, added top rated places using aggregations

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -51,9 +51,27 @@ exports.createStore = async (req, res) => {
 };
 
 exports.getStores = async (req, res) => {
+  const page = req.params.page || 1;
+  const limit = 4;
+  const skip = (page * limit ) - limit;
   // 1. Query the database for a list of all stores
-  const stores = await Store.find();
-  res.render('stores', { title: 'Stores', stores });
+  const storesPromise = Store
+    .find()
+    .skip(skip)
+    .limit(limit)
+    .sort({created: 'desc'});
+
+  const countPromise = Store.count();
+
+  const [stores, count] = await Promise.all([storesPromise, countPromise]);
+
+  const pages = Math.ceil(count / limit);
+  if(!stores.length && skip){
+    req.flash('info', `Hey! you asked for page ${page}. But that doesn't exist. So I put you on page ${pages}`);
+    res.redirect(`/stores/page/${pages}`);
+    return;
+  }
+  res.render('stores', { title: 'Stores', stores, page, pages, count });
 };
 
 const confirmOwner = (store, user) => {
@@ -86,7 +104,7 @@ exports.updateStore = async (req, res) => {
 };
 
 exports.getStoreBySlug = async (req, res, next) => {
-  const store = await Store.findOne({ slug: req.params.slug }).populate('author');
+  const store = await Store.findOne({ slug: req.params.slug }).populate('author reviews');
   if (!store) return next();
   res.render('store', { store, title: store.name });
 };
@@ -161,4 +179,10 @@ exports.getHearts = async(req, res) => {
     _id: {$in: req.user.hearts}
   });
   res.render('stores', {title: 'Hearted stores', stores});
+};
+
+exports.getTopStores = async(req, res) => {
+  const stores = await Store.getTopStores();
+  // res.json(stores);
+  res.render('topStores', {stores, title: '★ Top Stores!'});
 };
